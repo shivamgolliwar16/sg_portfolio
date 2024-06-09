b4992323d5f88c84a07402f3449f6570f3f1fc06
Shivam Golliwar <79265408+shivamgolliwar16@users.noreply.github.com>
2024-06-09 22:26:45 +0530

    added files

---
+++ script.js
@@ -0,0 +1,33 @@
+var i = 0;
+var textlap = '. Hello, I am Sanket Dudhmande...';
+var speedlap = 130;
+
+function typeWriterlap() {
+  if (i < textlap.length) {
+    document.getElementById("demolap").innerHTML += textlap.charAt(i);
+    i++;
+    setTimeout(typeWriterlap, speedlap);
+  }
+}
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+// Hi there! I am a final year IT student with expertise in programming and web development. I am passionate about creating innovative solutions and love tackling coding challenges. I am eager to bring my skills to a dynamic team and contribute to exciting projects.
+
